78279f85f9416e1dda36ef88fbd463f571f1daca
FayChik <145503677+FayChik@users.noreply.github.com>
2023-12-16 09:55:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ current_version = '1.1.1'
 import requests
 
 
-def download_update(url, file_path, github_version):
-    response = requests.get(url)
-    if response.status_code == 200:
-        text_code = response.content.decode('utf-8')
+def download_update(url, file_path, text_code):
+    try:
         with open(file_path, 'wb') as file:
-            text_code = f"\ncurrent_version = '{github_version}'\n\n{text_code}"
-            file.write(text_code.encode('utf-8'))
+            file.write(text_code)
 
-        print("Обновление загружено успешно.")
-        return True
-    else:
+            print("Обновление загружено успешно.")
+            return True
+    except BaseException as err:
+        print(err)
         print("Ошибка загрузки обновления.")
         return False
 
@@ -24,22 +22,32 @@ def check_for_updates(repo_owner, repo_name, file_path, current_version):
 
     if response.status_code == 200:
         content = response.json()
-        github_version = content['sha']  # Пример использования хэша SHA файла как версии
-        if github_version != current_version:
-            print("Найдено новое обновление.")
-            download_url = content['download_url']
-            update_downloaded = download_update(download_url, file_path, github_version)
+        download_url = content['download_url']
+        response = requests.get(download_url)
+        for line in response.content.decode('utf-8').split('\n'):
+            print(line)
+            if line.startswith('current_version ='):
+                version = sum([int(i) for i in current_version.split('.')])
+                github_version = sum([int(i) for i in line.replace('current_version =', '').replace('\'', '').replace('"', '').replace(' ', '').split('.')])
+                print(github_version)
+                print(version)
 
-            if update_downloaded:
-                # После успешной загрузки обновления
-                # можно добавить код для применения обновления, например:
-                # перезапуск приложения или обновление файлов
-                # ...
-                print("Обновление успешно применено.")
+                if version < github_version:
+
+                    print("Найдено новое обновление.")
+                    update_downloaded = download_update(download_url, file_path, response.content)
+
+                    if update_downloaded:
+                        # После успешной загрузки обновления
+                        # можно добавить код для применения обновления, например:
+                        # перезапуск приложения или обновление файлов
+                        # ...
+                        print("Обновление успешно применено.")
+                    else:
+                        print("Не удалось применить обновление.")
+                break
             else:
-                print("Не удалось применить обновление.")
-        else:
-            print("Установлена последняя версия.")
+                print("Установлена последняя версия.")
     else:
         print("Ошибка при получении информации с GitHub.")
 
@@ -53,9 +61,11 @@ file_path = "main.py"
 def main():
     print(2, 3)
     print('Vuaaaaaa')
+    print('Foeewf')
 
 
 if __name__ == '__main__':
     check_for_updates(repo_owner, repo_name, file_path, current_version)
     main()
+
 
